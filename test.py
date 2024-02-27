c5dffded76f80af874dff417204ffb834f4bdf94
LuisGiraldo <luisggon@protonmail.com>
2024-02-27 11:14:09 +0100

    script to test developments

---
+++ test.py
@@ -0,0 +1,6 @@
+from classes.SampleQC import SampleQC
+
+INPUT_PATH = '/mnt/0A2AAC152AABFBB7/PipeLine/data/inputData'
+INPUT_NAME = 'test_1'
+OUTPUT_PATH= '/mnt/0A2AAC152AABFBB7/PipeLine/data/outputData'
+CONFIG_PATH= '/mnt/0A2AAC152AABFBB7/comrare-pipeline/config.JSON'
