016a7066a8c6453916c4dc65a875941c332755dd
LuisGiraldo <luisggon@protonmail.com>
2024-02-27 14:04:44 +0100

    import fixed

--- classes/Helpers.py
+++ classes/Helpers.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import os
 import pandas as pd
 import numpy as np
 
--- test.py
+++ test.py
@@ -17,3 +17,5 @@ sample_QC = SampleQC(
 )
 
 sample_QC.run_ld_prune(ld_region_path=REGION_PATH)
+
+sample_QC.run_heterozygosity_rate()
