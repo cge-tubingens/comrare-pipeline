94aecc36e78e3ea63e6496b96eabf20a69ddd86e
LuisGiraldo <luisggon@protonmail.com>
2024-02-28 11:09:16 +0100

    minor style change

--- classes/Helpers.py
+++ classes/Helpers.py
@@ -13,7 +13,7 @@ def shell_do(command, print_cmd=False, log=False, return_log=False, err=False):
     if print_cmd:
         print(f'Executing: {(" ").join(command.split())}', file=sys.stderr)
 
-    res=subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     output = res.stdout.decode('utf-8') + res.stderr.decode('utf-8')
 
