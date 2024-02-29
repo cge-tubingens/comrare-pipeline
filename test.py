766b4b7421ab90319f78e246eed20542f75cb217
LuisGiraldo <luisggon@protonmail.com>
2024-02-29 14:09:32 +0100

    minor update

--- test.py
+++ test.py
@@ -22,15 +22,4 @@ sample_QC.run_heterozygosity_rate()
 
 sample_QC.run_sex_check()
 
-df_dup, df_imiss, df_1, df_2, removed = sample_QC.run_relatedness_prune()
-
-#df_dup
-#
-#df_imiss
-#
-#df_1
-#
-#df_2
-
-removed
-
+sample_QC.run_relatedness_prune()
