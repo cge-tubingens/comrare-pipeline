78b40eef2d3a2351c93c8ea0e58cfcfef26edcdf
LuisGiraldo <luisggon@protonmail.com>
2024-03-10 00:12:39 +0100

    test of new function

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ OUTPUT_PATH= '/mnt/0A2AAC152AABFBB7/PipeLine/data/outputData'
 OUTPUT_NAME= 'results'
 CONFIG_PATH= '/mnt/0A2AAC152AABFBB7/comrare-pipeline/config.JSON'
 
-REGION_PATH = '/mnt/0A2AAC152AABFBB7/PipeLine/data/high-LD-regions_GRCh37.txt'
+REGION_PATH = '/mnt/0A2AAC152AABFBB7/PipeLine/data/high-LD-regions.txt'
 
 sample_QC = SampleQC(
     input_path=INPUT_PATH,
@@ -23,3 +23,5 @@ sample_QC.run_heterozygosity_rate()
 sample_QC.run_sex_check()
 
 sample_QC.run_relatedness_prune()
+
+sample_QC.delete_failing_QC()
