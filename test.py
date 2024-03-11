32cb838e920da3590436f206bd4d1eef32210516
LuisGiraldo <luisggon@protonmail.com>
2024-03-11 13:40:06 +0100

    test newest function

--- test.py
+++ test.py
@@ -5,18 +5,18 @@ INPUT_NAME = 'test_1'
 OUTPUT_PATH= '/mnt/0A2AAC152AABFBB7/PipeLine/data/outputData'
 OUTPUT_NAME= 'results'
 CONFIG_PATH= '/mnt/0A2AAC152AABFBB7/comrare-pipeline/config.JSON'
-
-REGION_PATH = '/mnt/0A2AAC152AABFBB7/PipeLine/data/high-LD-regions.txt'
+DEPEND_PATH= '/mnt/0A2AAC152AABFBB7/PipeLine/data/auxiliarData'
 
 sample_QC = SampleQC(
     input_path=INPUT_PATH,
     input_name=INPUT_NAME,
     output_path=OUTPUT_PATH,
     output_name=OUTPUT_NAME,
-    config_path=CONFIG_PATH
+    config_path=CONFIG_PATH,
+    dependables_path=DEPEND_PATH
 )
 
-sample_QC.run_ld_prune(ld_region_path=REGION_PATH)
+sample_QC.run_ld_prune(ld_region_file='high-LD-regions.txt')
 
 sample_QC.run_heterozygosity_rate()
 
@@ -25,3 +25,8 @@ sample_QC.run_sex_check()
 sample_QC.run_relatedness_prune()
 
 sample_QC.delete_failing_QC()
+
+sample_QC.divergent_ancestry_step_one(
+    hapmap3r2_no_at_cg_snps_file='hapmap3r2_CEU.CHB.JPT.YRI.no-at-cg-snps.txt', 
+    hapmap3r2_founders_name='hapmap3r2_CEU.CHB.JPT.YRI.founders.no-at-cg-snps'
+)
